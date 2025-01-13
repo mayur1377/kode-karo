2694897656710c938d166a80ebed763708b1509a
MAYUR <mayuresh1377@gmail.com>
2025-01-14 00:50:35 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Years from "@/components/Years";
+
+export default function Home() {
+  return (
+    <div className="h-[50rem] w-full bg-black bg-dot-white/[0.2] relative flex items-center justify-center">
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+        <Years />
+      </p>
+    </div>
+  );
+}
